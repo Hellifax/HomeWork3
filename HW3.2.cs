46434ab0754ce35deffbc2633c5f231000d378a5
Hellifax <hellifax91@gmail.com>
2021-05-11 22:51:04 +0300

    Done tasks

---
+++ HW3.2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HW3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово:");
+            char[] arr = Console.ReadLine().ToCharArray();
+            var a = arr.Length;
+            Console.WriteLine("Кол-во элементов в массиве: {0}", a);
+            Array.Reverse(arr);
+            Console.WriteLine(arr);
+
+
+
+
+
+        }
+    }
+}
---
+++ HW3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HW3
+{
+    class HW3
+    {
+        static void Main(string[] args)
+        {
+            int[,] num = { { 1, 2 }, { 3, 4} };
+            Console.WriteLine($"{num[0, 0]}, {num[1, 1]}");
+
+
+        }
+    }
+}
